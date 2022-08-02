791738ff8799e4b5fab46891b349518e7c3c44bb
bpw <150335737@qq.com>
2022-08-02 17:57:17 +0800

    框架提交

--- app.js
+++ app.js
@@ -1,19 +1,5 @@
-// app.js
 App({
-  onLaunch() {
-    // 展示本地存储能力
-    const logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
+  onLaunch: function () {
 
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-  },
-  globalData: {
-    userInfo: null
   }
 })
